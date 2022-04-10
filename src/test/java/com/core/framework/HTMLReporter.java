47509eb31c1765ec581854c08a795e5f43112acb
Arvind Choudhary <arvindchoudhary142@yahoo.in>
2022-04-10 22:40:30 +0530

    extent-xml config presence check added.

--- src/test/java/com/core/framework/HTMLReporter.java
+++ src/test/java/com/core/framework/HTMLReporter.java
@@ -6,6 +6,7 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,46 +22,48 @@ public class HTMLReporter {
         htmlReporter = new ExtentSparkReporter(reportingFolder + "/result.html");
         extentReport = new ExtentReports();
         htmlTestLogs = new HashMap<>();
-        try {
-            htmlReporter.loadXMLConfig("src/test/resources/extent-config.xml");
-        } catch (Exception e) {
-            //do Nothing go with default config
+        if (new File("src/test/resources/extent-config.xml").exists()) {
+            try {
+                htmlReporter.loadXMLConfig("src/test/resources/extent-config.xml");
+            } catch (Exception e) {
+                //do Nothing go with default config
+            }
+
         }
     }
 
-    public void onTestStart(String methodName){
+    public void onTestStart(String methodName) {
         ExtentTest test = extentReport.createTest(methodName);
         extentReport.attachReporter(htmlReporter);
         htmlTestLogs.put(methodName, test);
     }
-    public void log(String methodName,TestLog log){
-        if(!htmlTestLogs.containsKey(methodName)){
-           onTestStart(methodName);
-        }
-        ExtentTest extentTest = htmlTestLogs.get(methodName);
-        extentTest.log(log.getStatus(),log.toString());
-    }
-    public void log(String methodName, Status status, String log){
-        if(!htmlTestLogs.containsKey(methodName)){
+
+    public void log(String methodName, TestLog log) {
+        if (!htmlTestLogs.containsKey(methodName)) {
             onTestStart(methodName);
         }
         ExtentTest extentTest = htmlTestLogs.get(methodName);
-        extentTest.log(status,log);
+        extentTest.log(log.getStatus(), log.toString());
+    }
+
+    public void log(String methodName, Status status, String log) {
+        if (!htmlTestLogs.containsKey(methodName)) {
+            onTestStart(methodName);
+        }
+        ExtentTest extentTest = htmlTestLogs.get(methodName);
+        extentTest.log(status, log);
     }
 
 
-
-    public boolean stopReporting(){
+    public boolean stopReporting() {
         extentReport.flush();
         return true;
     }
 
 
-
-
-    public static HTMLReporter initializeReporting(String reportingFolderPath){
+    public static HTMLReporter initializeReporting(String reportingFolderPath) {
         File folder = new File(reportingFolderPath);
-        if(!folder.exists()){
+        if (!folder.exists()) {
             folder.mkdirs();
         }
         return new HTMLReporter(reportingFolderPath);
