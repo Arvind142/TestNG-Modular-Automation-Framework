c629437e93337c53a3d276d0525f8c9dd8ddffc4
Arvind Choudhary <arvindchoudhary142@yahoo.in>
2022-12-26 19:16:55 +0530

    Constants updated

--- src/main/java/com/core/framework/constant/FrameworkConstants.java
+++ src/main/java/com/core/framework/constant/FrameworkConstants.java
@@ -1,6 +1,6 @@
 package com.core.framework.constant;
 
-public class FrameworkConst {
+public class FrameworkConstants {
     public static final String application_global_config = "src/test/resources/execution-config.properties";
     public static final String extent_config_xml = "src/test/resources/extent-config.xml";
     public static final String not_applicable_const = "NotApplicable";
